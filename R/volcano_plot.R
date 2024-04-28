734b31d2e9cbc8ef4eeb3e2fd47b20913ac89483
Fan fangzhou <haohaojiu@outlook.com>
2024-04-28 21:53:37 +0800

    jh

--- R/volcano_plot.R
+++ R/volcano_plot.R
@@ -1,6 +1,7 @@
 #' Return a volcano plot
 #'
 #' @param mydata The combine_results()  function returned a result
+#' @param adjust The choose of 'BH','FDR','None'
 #'
 #' @return Return a volcano plot
 #' @export
@@ -8,7 +9,7 @@
 #' @examples
 #' library(ggrepel)
 #' library(gt)
-volcano_plot <- function(mydata) {
+volcano_plot <- function(mydata,adjust='BH') {
   # 数据处理
   mydata <- mydata %>%
     dplyr::select(exposure, `b_Inverse variance weighted`, `pval_Inverse variance weighted`) %>%
@@ -22,54 +23,91 @@ volcano_plot <- function(mydata) {
     ylab("-log10(P-value)") +
     scale_x_continuous(limits = c(min(mydata$OR) - 0.1, max(mydata$OR) + 0.1)) +
     theme_minimal()
+
+
   # 数据处理
-  adjustedp <- 0.05 / nrow(mydata)
-  boni <- mydata %>%
-    filter(Pval < adjustedp)
-
-  mydata1 <- mydata %>%
-    mutate(expression = case_when(
-      OR >= 1 & Pval < 0.05 ~ "High risk",
-      OR <= 1 & Pval < 0.05 ~ "Low risk",
-      TRUE ~ "Insignificance"
-    ))
-
-  # 创建着色的火山图
-  volc_plot1 <- ggplot(mydata1, aes(OR, -log10(Pval))) +
-    geom_point(aes(color = expression), size = 1.4) +
-    xlab("OR") +
-    ylab("-log10(P-value)") +
-    scale_x_continuous(limits = c(min(mydata$OR) - 0.1, max(mydata$OR) + 0.1)) +
-    scale_color_manual(values = c("red1", "grey", "skyblue")) +
-    guides(color = guide_legend(title = "Risks(IVW method)")) +
-    # 添加分界线
-    geom_hline(yintercept = -log10(0.05), linetype = "dashed", color = "black") +
-    geom_hline(yintercept = -log10(adjustedp), linetype = "dashed", color = "black")
-
-
-  # 选择前十个数据
-  top_10 <- bind_rows(
-    mydata1 %>%
-      filter(Pval < 0.05) %>%
-      arrange(Pval, desc(abs(OR))) %>%
-      head()
-  )
-
-  # 在火山图上添加标签
-  volc_plot2 <- volc_plot1 +
-    geom_label_repel(
-      data = top_10,
-      aes(OR, -log10(Pval), label = ID),
-      size = 2
+  if(adjust=='BH'){
+    mydata1 <- mydata %>%
+      mutate(expression = case_when(
+        OR >= 1 & Pval < 0.05 ~ "High risk",
+        OR <= 1 & Pval < 0.05 ~ "Low risk",
+        TRUE ~ "Insignificance"
+      ))
+    # 选择前十个数据
+    top_10 <- bind_rows(
+      mydata1 %>%
+        filter(Pval < 0.05) %>%
+        arrange(Pval, desc(abs(OR))) %>%
+        head()
     )
+    adjustedp <- 0.05 / nrow(mydata)
+    boni <- mydata %>%
+      filter(Pval < adjustedp)
+    # 创建着色的火山图
+    volc_plot1 <- ggplot(mydata1, aes(OR, -log10(Pval))) +
+      geom_point(aes(color = expression), size = 1.4) +
+      xlab("OR") +
+      ylab("-log10(P-value)") +
+      scale_x_continuous(limits = c(min(mydata$OR) - 0.1, max(mydata$OR) + 0.1)) +
+      scale_color_manual(values = c("red1", "grey", "skyblue")) +
+      guides(color = guide_legend(title = "Risks(IVW method)")) +
+      # 添加分界线
+      geom_hline(yintercept = -log10(0.05), linetype = "dashed", color = "black") +
+      geom_hline(yintercept = -log10(adjustedp), linetype = "dashed", color = "black")
+    volc_plot2 <- volc_plot1 +
+      geom_label_repel(data = top_10,aes(OR, -log10(Pval), label = ID), size = 2)
+  }else if(adjust=='FDR'){
+    mydata$padj <- p.adjust(mydata$Pval,method='fdr')
+    mydata1 <- mydata %>%
+      mutate(expression = case_when(
+        OR >= 1 & padj < 0.05 ~ "High risk",
+        OR <= 1 & padj < 0.05 ~ "Low risk",
+        TRUE ~ "Insignificance"
+      ))
+    # 选择前十个数据
+    top_10 <- bind_rows(
+      mydata1 %>%
+        filter(padj < 0.05) %>%
+        arrange(padj, desc(abs(OR))) %>%
+        head()
+    )
+    volc_plot1 <- ggplot(mydata1, aes(OR, -log10(padj))) +
+      geom_point(aes(color = expression), size = 1.4) +
+      xlab("OR") +
+      ylab("-log10(Padj)") +
+      scale_x_continuous(limits = c(min(mydata$OR) - 0.1, max(mydata$OR) + 0.1)) +
+      scale_color_manual(values = c("red1", "grey", "skyblue")) +
+      guides(color = guide_legend(title = "Risks(IVW method)")) +
+      # 添加分界线
+      geom_hline(yintercept = -log10(0.05), linetype = "dashed", color = "black")
+    volc_plot2 <- volc_plot1 +
+      geom_label_repel(data = top_10,aes(OR, -log10(padj), label = ID), size = 2)
+  }else if(adjust=='None'){
+    mydata1 <- mydata %>%
+      mutate(expression = case_when(
+        OR >= 1 & Pval < 0.05 ~ "High risk",
+        OR <= 1 & Pval < 0.05 ~ "Low risk",
+        TRUE ~ "Insignificance"
+      ))
+    # 选择前十个数据
+    top_10 <- bind_rows(
+      mydata1 %>%
+        filter(Pval < 0.05) %>%
+        arrange(Pval, desc(abs(OR))) %>%
+        head()
+    )
+    volc_plot1 <- ggplot(mydata1, aes(OR, -log10(Pval))) +
+      geom_point(aes(color = expression), size = 1.4) +
+      xlab("OR") +
+      ylab("-log10(P-value)") +
+      scale_x_continuous(limits = c(min(mydata$OR) - 0.1, max(mydata$OR) + 0.1)) +
+      scale_color_manual(values = c("red1", "grey", "skyblue")) +
+      guides(color = guide_legend(title = "Risks(IVW method)")) +
+      # 添加分界线
+      geom_hline(yintercept = -log10(0.05), linetype = "dashed", color = "black")
+    volc_plot2 <- volc_plot1 +
+      geom_label_repel(data = top_10,aes(OR, -log10(Pval), label = ID), size = 2)
+  }
 
-  plot_list <- list(
-    "volcano_plot" = volc_plot,
-    "colored_volcano_plot" = volc_plot2,
-    "head_data" = head(mydata) %>% gt(),
-    "head_processed_data" = head(mydata1) %>% gt(),
-    "top_10_data" = top_10 %>% gt()
-  )
-
-  return(plot_list[["colored_volcano_plot"]])
+  return(volc_plot2)
 }
